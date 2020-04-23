5ba11a497d87d971a1ed37a4cc43e1b5fc7775d3
Sophia Law <sophialaw927@gmail.com>
2020-04-22 20:30:41 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let car = {
+    year: "2015",
+    make: "Ford",
+    model: "Mustang",
+    color: "red"
+}
+
+const shelterAnimals = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"];
+
+let mom = {
+    age: 63,
+    relation: "mom",
+    fave_color: "green",
+    fave_food: "hot dogs"
+}
+
+let dad = {
+    age: 63,
+    relation: "dad",
+    fave_color: "orange",
+    fave_food: "food"
+}
+
+let familyMembers = [mom, dad];
+
+
+
